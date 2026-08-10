bdd6f65f3ee2ef43b84df2dbe99a406b70c0388c
dino1051 <daniels.salinas.condor@gmail.com>
2026-08-10 16:26:10 -0600

    first commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+//import readline from 'readline/promises';
+//import { stdin as input, stdout as output } from 'process';
+
+//const rl = readline.createInterface({ input, output });
+// 🚫 No eliminar las líneas de arriba ⬆️
+
+// ✍️ Escribe tu código aquí 👇
+let systemName: string = "Windows 11";
+let version: number = 23.2 ;
+let userName: string = "Daniel S";
+
+const lines: string = "\n==================================\n"
+const msg00: string = 'Nombre del sistema v'
+const msg01: string = '¡Bienvenido, '
+
+console.log(lines+msg00+version+"\n"+msg01+userName+"!"+lines)
+
+// 🚫 No eliminar las líneas de abajo ⬇️
+//rl.close();
