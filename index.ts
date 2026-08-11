0a31131333c73d4bdb5648d4436a304bdd1a25a9
dino1051 <daniels.salinas.condor@gmail.com>
2026-08-11 16:36:56 -0600

    feat: menu interactivo con ciclo y manejo de arreglo

--- index.ts
+++ index.ts
@@ -12,8 +12,41 @@ let userName: string = "Daniel S";
 const lines: string = "\n==================================\n"
 const msg00: string = 'Nombre del sistema v'
 const msg01: string = '¡Bienvenido, '
-
+let opcion: number = 0;
+let titulos: string[] = []
+let status: boolean = true
 console.log(lines+msg00+version+"\n"+msg01+userName+"!"+lines)
+
+while(status){
+
+    let ans = await rl.question("")
+    console.log(`${lines} Qué deseas hacer?
+        1. Agregar el título de un libro
+        2. Eliminar el título del último libro agregado
+        3. Mostrar todos los títulos de libros
+        4. Salir del programa${lines}`)
+    
+    switch (parseInt(ans)){
+        case 1:
+            let libro = await rl.question("Ingresa el titulo: ");
+            titulos.push(libro);
+            console.log(`Se ha agregado ${libro} a la lista`);
+        case 2:
+            console.log(`Se ha eliminado ${titulos[titulos.length]} de la lista`);
+            titulos.pop();
+        case 3:
+            for (let i = 0;i < titulos.length;i++) {
+                console.log(`${i}: ${titulos[i]}`);                
+            }
+        case 4:
+            status = false;
+            console.log(`Buen día, ${userName}, hasta pronto.`)
+            break;
+        default:
+            console.log('Opcion no valida, vuelve a intentarlo')
+    }
+
+}
 
 // 🚫 No eliminar las líneas de abajo ⬇️
 rl.close();
